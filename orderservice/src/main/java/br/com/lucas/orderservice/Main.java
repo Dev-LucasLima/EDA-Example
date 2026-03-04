bdf6d319a00520a842f42a50ea556ba1eea65d34
Dev-LucasLima <lucas.lima.1478@outlook.com>
2026-03-03 21:13:37 -0300

    feat: create project to create orders

---
+++ orderservice/src/main/java/br/com/lucas/orderservice/Main.java
@@ -0,0 +1,10 @@
+package br.com.lucas.orderservice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(final String[] args) {
+        log.info("Hello world!");
+    }
+}
